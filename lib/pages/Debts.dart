5808d562944fc9334cb58350a2ecd99c8b84033d
DamirAhm <damir221003@gmail.com>
2021-08-04 19:10:58 +0400

    Added paid section to debts page

--- lib/pages/Debts.dart
+++ lib/pages/Debts.dart
@@ -74,26 +74,62 @@ class _DebtsPageState extends State<DebtsPage> {
         });
   }
 
+  List<Widget> _getActiveDebtTiles() {
+    final debtsController =
+        Provider.of<DebtsController>(context, listen: false);
+
+    return ListTile.divideTiles(
+            context: context,
+            tiles: debtsController.debts
+                .where((debt) => debt.amount != 0)
+                .map(
+                    (debt) => DebtTile(debt, () => _openDebtEditingModal(debt)))
+                .toList())
+        .toList();
+  }
+
+  List<Widget> _getPaidDebtTiles() {
+    final debtsController =
+        Provider.of<DebtsController>(context, listen: false);
+
+    return ListTile.divideTiles(
+            context: context,
+            tiles: debtsController.debts
+                .where((debt) => debt.amount == 0)
+                .map(
+                    (debt) => DebtTile(debt, () => _openDebtEditingModal(debt)))
+                .toList())
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<DebtsController>(
         builder: (context, debtsController, child) => MyScaffold(
-            title: 'Траты',
-            actions: [
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: _openNewDebtModal,
-              )
-            ],
-            body: Container(
-                child: ListView(
-                    children: ListTile.divideTiles(
-                            context: context,
-                            tiles: debtsController.debts
-                                .map((debt) => DebtTile(
-                                    debt, () => _openDebtEditingModal(debt)))
-                                .toList())
-                        .toList()))));
+              title: 'Долги',
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _openNewDebtModal,
+                )
+              ],
+              body: Container(
+                  child: ListView(children: [
+                ..._getActiveDebtTiles(),
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.symmetric(
+                            horizontal: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                                width: 1))),
+                    child: ListTile(
+                        title: Text('Выплаченные',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Theme.of(context).primaryColor)))),
+                ..._getPaidDebtTiles()
+              ])),
+            ));
   }
 }
 
